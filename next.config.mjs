b5b57add6e5bfea27bd28b39187f9b2a04db5b04
varsha6383 <varsha.zenthtech@gmail.com>
2025-05-27 14:48:58 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+    output:'export',
+};
 
-}
- 
-module.exports = nextConfig
+export default nextConfig;
